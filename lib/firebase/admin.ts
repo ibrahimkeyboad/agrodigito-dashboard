73b9d2ffe2e8ed8178f103e2eeafe8914883472b
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2026-02-03 16:07:47 +0300

    refactor: Update Firebase environment variable access to use `NEXT_PUBLIC_` prefix for client configuration and add a fallback for the admin project ID.

--- lib/firebase/admin.ts
+++ lib/firebase/admin.ts
@@ -29,7 +29,9 @@ export function createFirebaseAdminApp(params: FirebaseAdminParams) {
 
 export async function initAdmin() {
   const params = {
-    projectId: process.env.FIREBASE_PROJECT_ID!,
+    projectId:
+      process.env.FIREBASE_PROJECT_ID ||
+      process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
     privateKey: process.env.FIREBASE_PRIVATE_KEY!,
   };
